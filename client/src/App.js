9dcc2f53ff42aa0782e7780b980bde554d35bfa5
Rosa Turner <rmturner1220@gmail.com>
2022-09-30 16:12:24 -0400

    components updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,74 @@
-import Navbar from './components/Navbar'
-import Header from './components/Header'
-import Custom from './components/Custom'
-import Footer from './components/Footer'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import './App.css'
-
-export default function App() {
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+// import SingleWorkout from './pages/SingleWorkout';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+function App() {
   return (
-    <>
-      <Navbar />
-      <Header />
-      <Custom />
-      <Footer />
-    </>
-  )
+    <ApolloProvider client={client}>
+      <Router>
+        <Header />
+
+        <Routes>
+          <Route
+            path="/"
+            element={<Home />}
+          />
+          <Route
+            path="/login"
+            element={<Login />}
+          />
+          <Route
+            path="/signup"
+            element={<Signup />}
+          />
+          <Route
+            path="/profile"
+            element={<Profile />}
+          />
+          {/* <Route
+                path="/workout/:id"
+                element={<SingleWorkout />}
+              /> */}
+          <Route
+            path="*"
+            element={<NoMatch />}
+          />
+        </Routes>
+
+        <Footer />
+      </Router>
+
+    </ApolloProvider>
+  );
 }
+export default App;
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,8 +1,38 @@
 import HeaderImg from '../../images/header02.jpg'
+import custom01 from '../../images/custom01.jpg'
+import custom02 from '../../images/custom02.jpg'
+import custom03 from '../../images/custom03.jpg'
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Auth from '../../utils/auth';
 
 const Header = () => {
+    const logout = event => {
+        event.preventDefault();
+        Auth.logout();
+    };
     return (
-        <section>
+        <>
+            <nav className='navbar navbar-expand-sm fixed-top'>
+                <a href='/' className='navbar-brand'><span>H</span>ygieia Fitness</a>
+
+                {Auth.loggedIn() ? (
+                    <>
+                        <Link to="/profile">Me</Link>
+                        <a href="/" onClick={logout}>
+                            Logout
+                        </a>
+                    </>
+                ) : (
+                    <div className='nav-item'>
+                        <Link className='nav-link' to="/">Home</Link>
+                        <Link className='nav-link' to="/login">Login</Link>
+                        <Link className='nav-link' to="/signup">Signup</Link>
+                    </div>
+                )}
+            </nav>
+
             <div className='header'>
                 <div>
                     <div className='img'>
@@ -15,7 +45,33 @@ const Header = () => {
                     <button className='btn'>Join Now</button>
                 </div>
             </div>
-        </section>
-    )
-}
-export default Header 
+
+            <section>
+                <div className='custom container'>
+                    <div className='row'>
+                        <div className='col-sm-4'>
+                            <div className='box'>
+                                <img src={custom01} className='img-fluid' alt='' />
+                            </div>
+                        </div>
+
+                        <div className='col-sm-4'>
+                            <div className='box'>
+                                <img src={custom02} className='img-fluid' alt='' />
+                            </div>
+                        </div>
+
+                        <div className='col-sm-4'>
+                            <div className='box'>
+                                <img src={custom03} className='img-fluid' alt='' />
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+};
+
+export default Header;
