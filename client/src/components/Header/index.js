7a58eb549201ca062aa5726976af35d0a7cf16ce
Rosa Turner <rmturner1220@gmail.com>
2022-10-02 21:50:02 -0400

    hamburger menu

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import Auth from '../../utils/auth';
 import { Link } from 'react-router-dom';
 import './header.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
 
 const Header = () => {
 
@@ -9,33 +13,48 @@ const Header = () => {
         event.preventDefault();
         Auth.logout();
     };
+
     return (
 
-        <nav className='navbar navbar-expand-sm fixed-top'>
-            <a href='/' className='navbar-brand'><span>H</span>ygieia Fitness</a>
-            {Auth.loggedIn() ? (
-                <>
-                    <div className='navbar-brand'>
-                        <Link to="/profile">Me</Link>
-                        <a href="/" onClick={logout}>
-                            Logout
-                        </a>
-                    </div>
-                </>
-            ) : (
-                <div className='navbar-nav'>
-                    <Link className='nav-link' to="/Homepage">Home</Link>
-                    <Link className='nav-link' to="/profile">Profile</Link>
-                    <Link className='nav-link' to="/login">Login</Link>
-                    <Link className='nav-link' to="/signup">Signup</Link>
-                </div>
+        <Navbar bg="dark" expand="lg" className='navbar navbar-expand-sm fixed-top'>
+            <Container>
+                <Navbar.Brand href='/' className='navbar-brand'><span>H</span>ygieia Fitness</Navbar.Brand>
+                {Auth.loggedIn() ? (
 
-            )};
-        </nav>
+                    <>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="me-auto">
 
+                                <Link to="/profile">Me</Link>
+                                <a href="/" onClick={logout}>
+                                    Logout
+                                </a>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </>
 
+                ) : (
+
+                    <>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="me-auto">
+                                <Link className='nav-link' to="/">Home</Link>
+                                <Link className='nav-link' to="/profile">Profile</Link>
+                                <Link className='nav-link' to="/login">Login</Link>
+                                <Link className='nav-link' to="/signup">Signup</Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </>
+
+                )}
+
+            </Container>
+        </Navbar>
     );
 };
+
 
 export default Header;
 
