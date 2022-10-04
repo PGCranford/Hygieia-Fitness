de214da7fd0db2483ce6312ba8fdd352868d5f31
Rosa Turner <rmturner1220@gmail.com>
2022-10-04 19:15:42 -0400

    rosa

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -5,9 +5,17 @@ import './header.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-
+import { useState, useEffect } from "react";
 
 const Header = () => {
+
+    const [open, setOpen] = useState(false)
+
+    useEffect(() => {
+        if (!open) {
+            document.getElementById("close-button").click()
+        }
+    }, [open]);
 
     const logout = event => {
         event.preventDefault();
@@ -15,8 +23,8 @@ const Header = () => {
     };
 
     return (
-
         <Navbar bg="dark" expand="lg" className='navbar navbar-expand-sm fixed-top'>
+
             <Container>
                 <Navbar.Brand href='/' className='navbar-brand'><span>H</span>ygieia Fitness</Navbar.Brand>
                 {Auth.loggedIn() ? (
@@ -36,24 +44,25 @@ const Header = () => {
                 ) : (
 
                     <>
-                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                        <Navbar.Collapse id="basic-navbar-nav">
-                            <Nav className="me-auto">
-                                <Link className='nav-link' to="/">Home</Link>
-                                <Link className='nav-link' to="/profile">Profile</Link>
-                                <Link className='nav-link' to="/login">Login</Link>
-                                <Link className='nav-link' to="/signup">Signup</Link>
-                            </Nav>
-                        </Navbar.Collapse>
+                        <label onClick={() => setOpen(true)} id="close-button" htmlFor="nav" className="nav-btn">
+                            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                            <Navbar.Collapse id="basic-navbar-nav">
+                                <Nav className="me-auto">
+                                    <Link onClick={() => setOpen(false)} className='nav-link' to="/">Home</Link>
+                                    <Link onClick={() => setOpen(false)} className='nav-link' to="/login">Login</Link>
+                                    <Link onClick={() => setOpen(false)} className='nav-link' to="/signup">Signup</Link>
+                                </Nav>
+                            </Navbar.Collapse>
+                        </label>
                     </>
-
-                )}
+                )};
 
             </Container>
+
         </Navbar>
+
     );
 };
-
 
 export default Header;
 
