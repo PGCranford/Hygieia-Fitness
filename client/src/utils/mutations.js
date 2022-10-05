d3336e3c719d3071898a9c7158fefdc489b02b8d
Jason Oliver <58096660+joliver521@users.noreply.github.com>
2022-10-05 19:19:24 -0400

    updated files

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,43 +1,32 @@
-import { gql } from "@apollo/client";
+import { gql } from '@apollo/client';
 
 //login mutation
 export const LOGIN_USER = gql`
-  mutation login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
-      token
-      user {
-        _id
-        username
-      }
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
     }
-  }
 `;
 
 //signup mutation
 export const ADD_USER = gql`
-  mutation addUser($username: String!, $email: String!, $password: String!) {
-    addUser(username: $username, email: $email, password: $password) {
-      token
-      user {
-        _id
-        username
-      }
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
     }
-  }
 `;
 
 //add a workout
-export const ADD_WORKOUT = gql`
-  mutation addWorkout($workoutText: String!) {
-    addWorkout(workoutText: $workoutText) {
-      _id
-      workoutText
-      createdAt
-      username
-    }
-  }
-`;
-
 export const ADD_WORKOUT = gql`
     mutation addWorkout($workoutText: String!) {
         addWorkout(workoutText: $workoutText) {
@@ -45,10 +34,6 @@ export const ADD_WORKOUT = gql`
             workoutText
             createdAt
             username
-            commentCount
-            comments {
-                _id
-            }
         }
     }
 `;
@@ -62,33 +47,6 @@ export const ADD_COMMENT = gql`
                 _id
                 commentBody
                 createdAt
-                username
-            }
-        }
-    }
-`;
-
-export const ADD_FRIEND = gql`
-    mutation addFriend($id: ID!) {
-        addFriend(friendId: $id) {
-            _id
-            username
-            friendCount
-            friends {
-                _id
-                username
-            }
-        }
-    }
-`;
-
-export const REMOVE_FRIEND = gql`
-    mutation removeFriend($id: ID!) {
-        removeFriend(id: $id) {
-            _id
-            username
-            friends {
-                _id
                 username
             }
         }
