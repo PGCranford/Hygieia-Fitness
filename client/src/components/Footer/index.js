899c4fa4863e46f836e0ba76c35fe7d507f01f3a
Rosa Turner <rmturner1220@gmail.com>
2022-10-06 13:57:47 -0400

    rosa

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import styles from "./style.module.css";
 
 
 const Footer = () => {
     return (
-        <footer className={styles["footer-main"]}> 
+        <footer>
             <div className="foot">&copy;2022 by Hygieia Fitness</div>
         </footer>
     );
