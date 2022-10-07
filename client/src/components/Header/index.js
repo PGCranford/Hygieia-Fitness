0fe18f9d57364aa79c7e955b866582f967c50ca7
Rosa Turner <rmturner1220@gmail.com>
2022-10-06 23:22:20 -0400

    Changed home style

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Auth from '../../utils/auth';
-import { Link } from 'react-router-dom';
 import './header.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
@@ -24,7 +23,7 @@ const Header = () => {
         <Navbar bg="dark" expand="lg" className='navbar navbar-expand-sm fixed-top'>
 
             <Container>
-                <Navbar.Brand href='/' className='navbar-brand'><span>H</span>ygieia Fitness</Navbar.Brand>
+                <Navbar.Brand href='/home' className='navbar-brand'><span>H</span>ygieia Fitness</Navbar.Brand>
                 {Auth.loggedIn() ? (
 
                     <>
@@ -32,7 +31,7 @@ const Header = () => {
                         <Navbar.Collapse id="basic-navbar-nav">
                             <Nav className="me-auto">
                                 <Nav.Link onClick={() => setOpen(false)} className='nav-link' aria-controls="basic-navbar-nav" href="/profile">Me</Nav.Link>
-                                <a className='nav-link' href="/" onClick={logout}>
+                                <a className='nav-link' href="/home" onClick={logout}>
                                     Logout
                                 </a>
                             </Nav>
@@ -46,7 +45,7 @@ const Header = () => {
                         <Navbar.Toggle aria-controls="basic-navbar-nav" />
                         <Navbar.Collapse id="basic-navbar-nav">
                             <Nav className="me-auto">
-                                <Nav.Link onClick={() => setOpen(false)} className='nav-link' aria-controls="basic-navbar-nav" href="/">Home</Nav.Link>
+                                <Nav.Link onClick={() => setOpen(false)} className='nav-link' aria-controls="basic-navbar-nav" href="/home">Home</Nav.Link>
                                 <Nav.Link onClick={() => setOpen(false)} className='nav-link' aria-controls="basic-navbar-nav" href="/login">Login</Nav.Link>
                                 <Nav.Link onClick={() => setOpen(false)} className='nav-link' aria-controls="basic-navbar-nav" href="/signup">Signup</Nav.Link>
                             </Nav>
