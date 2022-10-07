d1c200700ea801993472d94b4f1c834283e7ebfa
Austin Davis <austindavis@Austins-MBP.attlocal.net>
2022-10-07 01:01:04 -0400

    filled out SingleComment.js

--- client/src/pages/SingleComment.js
+++ client/src/pages/SingleComment.js
@@ -1,1 +1,48 @@
 //make sure authorization is added for when logged in 
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+import CommentList from '../components/CommentList';
+import CommentForm from '../components/CommentForm';
+
+import Auth from '../utils/auth';
+import { useQuery } from '@apollo/client';
+import { QUERY_COMMENT } from '../utils/queries';
+
+const SingleComment = (props) => {
+  const { id: commentId } = useParams();
+
+  const { loading, data } = useQuery(QUERY_COMMENT, {
+    variables: { id: commentId },
+  });
+
+  const comment = data?.comment || {};
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <div className="card mb-3">
+        <p className="card-header">
+          <span style={{ fontWeight: 700 }} className="text-light">
+            {comment.username}
+          </span>{' '}
+          comment on {comment.createdAt}
+        </p>
+        <div className="card-body">
+          <p>{comment.commentText}</p>
+        </div>
+      </div>
+
+      {comment.commentCount > 0 && (
+        <CommentList reactions={comment.comments} />
+      )}
+
+      {Auth.loggedIn() && <CommentForm thoughtId={comment._id} />}
+    </div>
+  );
+};
+
+export default SingleComment;
