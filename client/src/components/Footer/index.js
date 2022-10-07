dc80a657b0c4e48d16ad5a9cebcf6b3d87413b8b
PGCranford <PGCranford@gmail.com>
2022-10-07 09:54:14 -0400

    premerge

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,13 +1,11 @@
-import React from 'react';
-import styles from "./style.module.css";
-
+import React from "react";
 
 const Footer = () => {
-    return (
-        <footer className={styles["footer-main"]}> 
-            <div className="foot">&copy;2022 by Hygieia Fitness</div>
-        </footer>
-    );
+  return (
+    <footer>
+      <div className="foot">&copy;2022 by Hygieia Fitness</div>
+    </footer>
+  );
 };
 
 export default Footer;
